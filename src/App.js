1e8025c7687b3aa1e7ff594d818e2a3c98b31fa7
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-18 15:41:53 +0100

    updating repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import img from './527bb6490a176.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Card'>
+        <div className='Card-Section'>
+          <img className='Card-Image' src={img} alt='Silver Surfer' />
+          <div className='Card-Stats'>
+            <div className='Card-Stat'><span className='Card-Stat-Rating'>97</span><span>Durability</span></div>
+            <div className='Card-Stat'><span className='Card-Stat-Rating'>97</span><span>Energy</span></div>
+            <div className='Card-Stat'><span className='Card-Stat-Rating'>75</span><span>Fighting</span></div>
+            <div className='Card-Stat'><span className='Card-Stat-Rating'>64</span><span>Intelligence</span></div>
+            <div className='Card-Stat'><span className='Card-Stat-Rating'>97</span><span>Speed</span></div>
+            <div className='Card-Stat'><span className='Card-Stat-Rating'>86</span><span>Strength</span></div>
+          </div>
+        </div>
+        <div className='Card-Name-Wrapper'>
+          <div className='Card-Name'>Silver Surfer</div>
+        </div>
+      </div>
     </div>
   );
 }
