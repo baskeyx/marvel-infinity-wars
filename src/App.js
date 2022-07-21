c9ebb491b521dab5453dfb427e9a24efe535a8a2
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-21 13:26:46 +0100

    amplify set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import Card from './components/card';
+import { useState, useEffect } from 'react';
 import Game from './components/game';
 import './App.css';
+import { API } from 'aws-amplify'
+// import awsconfig from './aws-exports';
+// Auth.configure(awsconfig);
 
 function App() {
+  const [myMessage, setMyMessage] = useState('');
+  const apiCall = async () => {
+    API.get('AMPAPI', '/test', {})
+    .then(response => {
+      setMyMessage(response.success)
+    })
+  }
+  useEffect(() => {
+    apiCall();
+  }, []);
   return (
     <div className="App">
+      {myMessage}
       {/* <Card img={img} /> */}
       <Game />
     </div>
