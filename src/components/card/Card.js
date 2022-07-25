d1189969d2ccd85262a8e07943ffafb21ad704b5
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-25 12:43:11 +0100

    tweaks to card design

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,19 +2,19 @@ import styles from './Card.module.scss';
 
 const Card = () => (
   <div className={styles.Card}>
-    <div>
+    <div className={styles.CardInfo}>
       <img className={styles.CardImage} src='https://teamsupreme.s3.eu-west-2.amazonaws.com/public/527bb6490a176.webp' alt='Silver Surfer' />
-      <div className={styles.CardStats}>
-        <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Durability</span></div>
-        <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Energy</span></div>
-        <div className={styles.CardStat}><span className={styles.CardStatRating}>75</span><span>Fighting</span></div>
-        <div className={styles.CardStat}><span className={styles.CardStatRating}>64</span><span>Intelligence</span></div>
-        <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Speed</span></div>
-        <div className={styles.CardStat}><span className={styles.CardStatRating}>86</span><span>Strength</span></div>
+      <div className={styles.CardNameWrapper}>
+        <div className={styles.CardName}>Silver Surfer</div>
       </div>
     </div>
-    <div className={styles.CardNameWrapper}>
-      <div className={styles.CardName}>Silver Surfer</div>
+    <div className={styles.CardStats}>
+      <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Durability</span></div>
+      <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Energy</span></div>
+      <div className={styles.CardStat}><span className={styles.CardStatRating}>75</span><span>Fighting</span></div>
+      <div className={styles.CardStat}><span className={styles.CardStatRating}>64</span><span>Intelligence</span></div>
+      <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Speed</span></div>
+      <div className={styles.CardStat}><span className={styles.CardStatRating}>86</span><span>Strength</span></div>
     </div>
   </div>
 );
