abf42c26a48dfb766b1aa35ecb30ff432049e724
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-25 18:18:49 +0100

    adding button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import styles from './Button.module.scss';
+
+const Button = ({ children, ...props }) => (
+  <button className={styles.Button} {...props}>
+    {children}
+  </button>
+);
+
+export default Button;
--- src/views/intro/Intro.js
+++ src/views/intro/Intro.js
@@ -3,10 +3,11 @@ import Type from '../../components/Type';
 const Intro = () => (
   <>
     <Type phrases={[
-      'Hey Champ!',
-      'Welcome to Marvel Infinity Wars!',
-      "It’s me, your friendly neighbourhood Spider-Man!",
-
+      'Hey Champ! Welcome to Marvel Infinity Wars!',
+      'It’s me, your friendly neighbourhood Spider-Man!',
+      'I just thought I’d swing by & let you know how this works.',
+      'Firstly we’re gonna need to recruit a member to the team.',
+      'Click the ‘recruit’ tab in the bar below!'
       ]} />
   </>
 );
