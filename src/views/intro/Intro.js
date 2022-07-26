25e980a2cc76522b99279bad2b113a57c98f5ea6
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-26 09:49:48 +0100

    changing intro

--- src/views/intro/Intro.js
+++ src/views/intro/Intro.js
@@ -5,9 +5,9 @@ const Intro = () => (
     <Type phrases={[
       'Hey Champ! Welcome to Marvel Infinity Wars!',
       'It’s me, your friendly neighbourhood Spider-Man!',
-      'I just thought I’d swing by & let you know how this works.',
+      'I just thought I’d swing by & let you know how this thing works.',
       'Firstly we’re gonna need to recruit a member to the team.',
-      'Click the ‘recruit’ tab in the bar below!'
+      'Click the ‘recruit’ tab below!'
       ]} />
   </>
 );
