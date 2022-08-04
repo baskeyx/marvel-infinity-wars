99330d99e6a327039ddaea68a5e54f6f4be01760
baskeyx <daniel.baskeyfield@gmail.com>
2022-08-04 16:45:42 +0100

    packs

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -1,15 +1,11 @@
-import { useState, useEffect } from 'react';
 import styles from './Card.module.scss';
+import Button from '../Button';
 
-const Card = ({ character, revealCard }) => {
-  const [reveal, setReveal] = useState(revealCard);
-  useEffect(() => {
-    setTimeout(()=> setReveal(''), 2000);
-  }, []);
-  return (
-    <div className={`${styles.Card} ${styles[reveal]}`}>
+const Card = ({ character, setOpen, reveal, btnClick }) => (
+  <div className={styles[reveal]}>
+    <div className={`${styles.Card}`}>
       <div className={styles.CardInfo}>
-        <img className={styles.CardImage} src={`https://teamsupreme.s3.eu-west-2.amazonaws.com/public/${character.charId}.webp`} alt={character.name} />
+        <img className={styles.CardImage} src={`https://teamsupreme.s3.eu-west-2.amazonaws.com/public/${character.charId}.webp`} alt={character.name} onLoad={setOpen ? setOpen : null} />
         <div className={styles.CardNameWrapper}>
           <div className={styles.CardName}>{character.name}</div>
         </div>
@@ -23,37 +19,8 @@ const Card = ({ character, revealCard }) => {
         <div className={styles.CardStat}><span className={styles.CardStatRating}>{character.stats.str}</span><span>Strength</span></div>
       </div>
     </div>
-  )
-};
+    <Button onClick={btnClick}>OK</Button>
+  </div>
+)
 
 export default Card;
-
-// import { useState, useEffect } from 'react';
-// import styles from './Card.module.scss';
-
-// const Card = ({ revealCard }) => {
-//   const [reveal, setReveal] = useState(revealCard);
-//   useEffect(() => {
-//     setTimeout(()=> setReveal(''), 2000);
-//   }, []);
-//   return (
-//     <div className={`${styles.Card} ${styles[reveal]}`}>
-//       <div className={styles.CardInfo}>
-//         <img className={styles.CardImage} src='https://teamsupreme.s3.eu-west-2.amazonaws.com/public/527bb6490a176.webp' alt='Silver Surfer' />
-//         <div className={styles.CardNameWrapper}>
-//           <div className={styles.CardName}>Silver Surfer</div>
-//         </div>
-//       </div>
-//       <div className={styles.CardStats}>
-//         <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Durability</span></div>
-//         <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Energy</span></div>
-//         <div className={styles.CardStat}><span className={styles.CardStatRating}>75</span><span>Fighting</span></div>
-//         <div className={styles.CardStat}><span className={styles.CardStatRating}>64</span><span>Intelligence</span></div>
-//         <div className={styles.CardStat}><span className={styles.CardStatRating}>97</span><span>Speed</span></div>
-//         <div className={styles.CardStat}><span className={styles.CardStatRating}>86</span><span>Strength</span></div>
-//       </div>
-//     </div>
-//   )
-// };
-
-// export default Card;
